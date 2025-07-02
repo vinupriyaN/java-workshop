050365fa773b452a7d87334d15cceb780d4dc0bf
vinupriyaN <vinupriyan24cse@vetias.ac.in>
2025-07-02 14:49:23 +0530

    java program using array

---
+++ src/com/vetias/java/workshop/basics/array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[]args){
+        int[] marks = {90, 80, 70, 60, 50};
+        for (int mark : marks)
+         {
+           System.out.println(mark);
+        }
+    }
+
+}
