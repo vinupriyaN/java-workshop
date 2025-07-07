7c0a251cc539492d2af500542640746b52c53414
vinupriyaN <vinupriyan24cse@vetias.ac.in>
2025-07-07 10:42:18 +0530

    java

---
+++ src/com/vetias/java/workshop/basics/count.java
@@ -0,0 +1,13 @@
+public class count {
+    public static void main(String[] args) {
+        String  sentence= "this program show how can we split a string into multiple strings";
+        String[] words = sentence.split(" ");
+        int num=words.length;
+        System.out.println(num);
+        
+
+    
+    
+    }
+    
+}
