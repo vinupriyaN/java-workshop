44107fb0e80f53f2abc1323d65d9fe1e9628dcdd
vinupriyaN <vinupriyan24cse@vetias.ac.in>
2025-07-07 14:36:26 +0530

    append in java

---
+++ src/com/vetias/java/workshop/basics/Append.java
@@ -0,0 +1,16 @@
+public class Append {
+    public static void main(String[] args) {
+        StringBuilder sentence = new StringBuilder();
+        sentence.append("java");
+        sentence.append(" is ");
+        sentence.append("the ");
+        sentence.append("most ");
+        sentence.append("used ");
+        sentence.append("language ");
+        sentence.append("for ");
+        sentence.append("enterprise");
+        sentence.append(" applications.");
+        System.out.println(sentence.toString());
+    }
+    
+}
