ec1b26ba5a5e5e8162d6137785eac336cb79ce3a
vinupriyaN <vinupriyan24cse@vetias.ac.in>
2025-07-07 10:25:00 +0530

    string in java

---
+++ src/com/vetias/java/workshop/basics/Stringmethod.java
@@ -0,0 +1,13 @@
+public class Stringmethod {
+    public static void main(String[]args){
+        String firstname="vinu";
+        String lastname="priya";
+        String Name=firstname.concat(lastname);
+        System.out.println(Name);
+        String finalname=Name.toUpperCase();
+        System.out.println(finalname);
+
+    }
+
+    
+}
