575981c67fbadd0fe82db3166c83c07644f04eec
vinupriyaN <vinupriyan24cse@vetias.ac.in>
2025-07-08 10:44:38 +0530

    registration time left

---
+++ src/com/vetias/java/workshop/basics/Registrationtimeleft.java
@@ -0,0 +1,13 @@
+import java.time.LocalTime;
+
+public class Registrationtimeleft {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        LocalTime registrationendtime = LocalTime.of(18, 59, 59);
+        int hoursLeft=registrationendtime.getHour()- currentTime.getHour();
+        int minutesLeft=registrationendtime.getMinute()- currentTime.getMinute();
+        int secondsLeft=registrationendtime.getSecond()- currentTime.getSecond();
+        System.out.println("you have" + hoursLeft + " hours, " + minutesLeft + " minutes, " + secondsLeft + " seconds");
+         // 5:
+    }
+}
