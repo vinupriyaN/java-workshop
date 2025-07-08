b6ad707bc910dea2e96ca0bae3e0a0e4b42d3cd3
vinupriyaN <vinupriyan24cse@vetias.ac.in>
2025-07-08 10:44:57 +0530

    agecalculator

--- src/com/vetias/java/workshop/basics/Agecalculator.java
+++ src/com/vetias/java/workshop/basics/Agecalculator.java
@@ -1,13 +1,19 @@
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Agecalculator {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int currentYear =sc.nextInt();
-        int birthYear = sc.nextInt(); 
-        int age = currentYear - birthYear;
+        System.out.println("Enter the Date of birth(DD/MM/YY):");
+        String dateofbirth=sc.nextLine();
+        LocalDate birthdate = LocalDate.parse(dateofbirth);
+        int yearofbirth = birthdate.getYear();
+        int currentyear= LocalDate.now().getYear();
+        int age = currentyear - yearofbirth;
         System.out.println("Your age is: " + age);
+    
     }
     
 }
