6eead5a607abc1559d6b8bc257cf5697c247d811
vinupriyaN <vinupriyan24cse@vetias.ac.in>
2025-07-10 15:52:46 +0530

    map concept

---
+++ src/main/java/com/vetias/java/workshop/collection/StudentMap.java
@@ -0,0 +1,18 @@
+package com.vetias.java.workshop.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentMap {
+    public static void main(String[] args) {
+        Map<Integer, String> students= new HashMap<>();
+        students.put(101, "Alice");
+        students.put(102, "Bob");   
+        students.put(103, "Charlie");
+        students.put(104, "David");
+        students.put(105, "Eve");
+        System.out.println("Student Map: " + students);
+
+    }
+    
+}
